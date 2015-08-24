acfcc37a2f00ae20bf462fc6e966a0c4b5689fbe
minamorl <minamorl@users.noreply.github.com>
2015-08-25 04:48:55 +0900

    feat: replace clint by argparse

--- gitsampler/__main__.py
+++ gitsampler/__main__.py
@@ -2,15 +2,25 @@ import clint
 from gitsampler import messages
 from gitsampler import github
 from functools import partialmethod
+import argparse
 
 
 def main():
-    args = clint.Args()
-    repo_name = args.get(0)
-    messages.downloading(repo_name)
-    uri = github.extract_github_uri(repo_name)
-    github.clone_from(uri)
-    messages.done(repo_name)
+    parser = argparse.ArgumentParser(description="auto cloaning github repos")
+    parser.add_argument('--load')
+    parser.add_argument('repos', nargs='+')
+    
+    args = parser.parse_args()
+    if args.load:
+        github.clone_from_file(args.load)
+
+    else:
+        for repo_name in args.repos:
+            messages.downloading(repo_name)
+            uri = github.extract_github_uri(repo_name)
+            github.clone_from(uri)
+            messages.done(repo_name)
+
 
 if __name__ == "__main__":
     main()
