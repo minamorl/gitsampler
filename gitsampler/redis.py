1b666bc3686037a7b34d6f8787e5d80900a922ec
minamorl <minamorl@users.noreply.github.com>
2015-09-08 20:33:03 +0900

    fix: redis delete same prefix keys only

--- gitsampler/redis.py
+++ gitsampler/redis.py
@@ -5,7 +5,14 @@ REDIS_PREFIX = "gitsampler:commits:"
 def get_redis():
     return redis.StrictRedis()
 
+def delete_all(redis, prefix):
+    for key in redis.keys(REDIS_PREFIX + "*"):
+        redis.delete(key)
+
 def save_all(redis, iterable):
-    redis.flushall()
-    for dict in iterable:
-        redis.hmset(REDIS_PREFIX + ' '.join(dict.values()) ,dict)
+    try:
+        delete_all(redis, REDIS_PREFIX)
+        for dict in iterable:
+            redis.hmset(REDIS_PREFIX + '@'.join(dict.values()) ,dict)
+    except redis.exceptions.ResponseError:
+        print('Redis server is not ready. [Skip]')
