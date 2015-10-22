3e5d99b7aa0b20375fd358cebd45d1bdb11a716a
minamorl <minamorl@users.noreply.github.com>
2015-10-22 15:42:37 +0900

    feat: asynchronous download

--- gitsampler/__main__.py
+++ gitsampler/__main__.py
@@ -3,22 +3,26 @@ import sys, os
 from gitsampler import github, utils, messages, redis
 import json
 import io
+import asyncio
+import itertools
 
-def _main(args):
+
+async def task(repo_name):
+    messages.downloading(repo_name)
+    uri = github.extract_github_uri(repo_name)
+    repo = await github.clone_from(uri)
+    logs = list(github.read_log(repo_name, repo))
+    for l in logs:
+        print(l["summary"])
+    messages.done(repo_name)
+    return logs
+
+
+async def _main(args):
     
     mio = io.StringIO()
-    logs = []
-    for repo_name in args.repos:
-        messages.downloading(repo_name)
-        uri = github.extract_github_uri(repo_name)
-        repo = github.clone_from(uri)
-
-        for l in github.read_log(repo_name, repo):
-            logs.append(l)
-            print(l["summary"])
-
-        messages.done(repo_name)
-
+    coroutines = (task(repo_name) for repo_name in args.repos)
+    logs = list(itertools.chain(*await asyncio.gather(*coroutines)))
     r = redis.get_redis()
     redis.save_all(r, logs)
 
@@ -29,7 +33,10 @@ def main():
     if args.load:
         args.repos = utils.import_from_list(args.load)
 
-    return _main(args)
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_main(args))
+    loop.close()
 
 
 if __name__ == "__main__":
--- gitsampler/github.py
+++ gitsampler/github.py
@@ -16,15 +16,15 @@ def abbr_github_uri(uri):
     return parse("git@github.com:{user}/{repo}.git", uri)["repo"]
 
 
-def clone_from(repo_uri, dest=None):
+async def clone_from(repo_uri, dest=None):
     repo_name = abbr_github_uri(repo_uri)
     dest = dest or os.path.join(os.getcwd(), "misc/repos/", repo_name)
     if os.path.exists(dest):
         return git.Repo(dest)
-    return _clone_from(repo_uri, dest)
+    return await _clone_from(repo_uri, dest)
 
 
-def _clone_from(repo_uri, dest):
+async def _clone_from(repo_uri, dest):
     repo_name = abbr_github_uri(repo_uri)
     return git.Repo.clone_from(repo_uri, dest)
 
